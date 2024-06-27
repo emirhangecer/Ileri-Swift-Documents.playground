40ba3569377d2950d87240c497b686ea23de75fe
emirhangecer <emirhan19072121@gmail.com>
2024-06-28 02:27:27 +0300

    new code uptading

--- Contents.swift
+++ Contents.swift
@@ -23,3 +23,35 @@ func kisiTanıma2 (ad: String){
 }
 
 kisiTanıma2(ad: "Emirhan")
+
+//Hata Ayıklama
+
+// 1. Compile Error : Editör hataları
+// 2. Runtime Error : Exception : Çalışma sırasında oluşan hatalar
+
+enum Hatalar : Error {
+case SıfıraBolunmeHatasi
+}
+
+func bolme (sayi1: Int, sayi2 : Int) throws -> Int {
+    
+    if sayi2 == 0 {
+        throw Hatalar.SıfıraBolunmeHatasi
+    }
+    return sayi1 / sayi2
+}
+
+do {
+     let sonuc = try bolme(sayi1: 10, sayi2: 0)
+    print(sonuc)
+} catch Hatalar.SıfıraBolunmeHatasi {
+    print("Sayı sıfıra bölünemez")
+}
+
+let sonuc1 = try? bolme(sayi1: 10, sayi2: 0) //Eğer hata olursa sonuc nill olur
+
+if let temp = sonuc1 {
+    print(temp)
+} else {
+    print("Sayı Sıfıra Bölünemez")
+}
